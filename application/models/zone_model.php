ad589d9b436859c9a47bf5e02d67299b1448a65b
Saleh Saiid <saleh.saiid@gmail.com>
2014-07-25 17:36:21 +0300

    sort zone list by name ASC

--- application/models/zone_model.php
+++ application/models/zone_model.php
@@ -5,7 +5,23 @@ class zone_model extends MY_Model{
 	public $_table = "zone"; // Table name
 	public $primary_key = "zone_id"; // Table primary key
 
+	public $before_create = array( "timestampInsert" ); // observer before create row
+	public $before_update = array( "timestampUpdate" ); // observer before 
+
+	protected function timestampInsert($data){
+		$data["date_added"] = $data["date_modified"] = time();
+
+		return $data;
+	}
+
+	protected function timestampUpdate($data){
+		$data["date_modified"] = time();
+
+		return $data;
+	}
+
 	public function dropdown(){
+		$this->_database->order_by('zone_name' , 'ASC');
 		$list = parent::dropdown($this->primary_key, "zone_name");
 
 		return $list;
