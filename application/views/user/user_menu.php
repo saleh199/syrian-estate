c9bbf2b8447d6901b145949c008c6eea8d46cd0c
saleh199 <saleh.saiid@gmail.com>
2014-07-27 20:05:30 +0300

    seperate user menu

---
+++ application/views/user/user_menu.php
@@ -0,0 +1,9 @@
+			<div class="col-md-3 user-menu">
+                  <ul class="nav nav-pills nav-stacked">
+                        <li class="active"><a href="#">عقاراتي</a></li>
+                        <li><a href="#">معلومات الحساب</a></li>
+                        <li><a href="#">تغيير كلمة المرور</a></li>
+                        <li><a href="#">مشاريع عقارية</a></li>
+                        <li><a href="#">تسجيل خروج</a></li>
+                  </ul>
+            </div> <!-- /.user-menu -->
