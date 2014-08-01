c572b4375643818c8d5c18828194f4c8bc623b51
saleh199 <saleh.saiid@gmail.com>
2014-08-01 20:46:06 +0300

    add login page & enable login

---
+++ application/controllers/login.php
@@ -0,0 +1,65 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->library('ion_auth');
+		$this->load->library('form_validation');
+
+		$this->lang->load('auth');
+	}
+
+	public function index()
+	{
+		$data = array();
+
+		//validate form input
+		$this->form_validation->set_rules('email', 'البريد الإلكتروني', 'required');
+		$this->form_validation->set_rules('password', 'كلمة المرور', 'required');
+
+		if ($this->form_validation->run() == true)
+		{
+			$remember = TRUE;
+
+			if ($this->ion_auth->login($this->input->post('email'), $this->input->post('password'), $remember))
+			{
+				$this->session->set_flashdata('message', $this->ion_auth->messages());
+				redirect('/', 'refresh');
+			}
+			else
+			{
+				//if the login was un-successful
+				//redirect them back to the login page
+				$this->session->set_flashdata('message', $this->ion_auth->errors());
+				redirect('login', 'refresh'); //use redirects instead of loading views for compatibility with MY_Controller libraries
+			}
+		}else{
+			$data['message'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
+
+			$data["form_action"] = form_open("login", array("method" => "post"));
+
+			$data["input_email"] = form_input(array(
+				"type" => "text",
+				"name" => "email",
+				"class" => "form-control",
+				"placeholder" => "البريد الإلكتروني",
+				"value" => $this->form_validation->set_value('email')
+			));
+
+			$data["input_password"] = form_input(array(
+				"type" => "password",
+				"name" => "password",
+				"class" => "form-control",
+				"placeholder" => "كلمة المرور"
+			));
+
+		}
+
+		$this->load->view('login', $data);
+	}
+}
+
+/* End of file login.php */
+/* Location: ./application/controllers/login.php */
---
+++ application/views/property/view.php
@@ -0,0 +1,61 @@
+<?php echo $this->view("layouts/header", array("map" => false)); ?>
+
+      <div class="row main">
+
+      	<div class="col-md-12 property-view">
+                  <div class="col-md-9">
+                        <div class="page-header">
+                              <h4><?php echo $property_info->title;?></h4>
+                        </div>
+                        <div class="row">
+                              <div class="col-md-6">
+                                    <dl class="dl-horizontal">
+                                          <dt>السعر : </dt>
+                                          <dd><h4 class="price"><?php echo $property_info->price; ?> ل.س</h4></dd>
+
+                                          <dt>مساحة العقار : </dt>
+                                          <dd><b><?php echo $property_info->area; ?></b> متر مربع</dd>
+
+                                          <dt>عنوان العقار : </dt>
+                                          <dd><?php echo $property_info->address; ?></dd>
+
+                                    </dl>
+                              </div>
+                              <div class="col-md-6">
+                                    <div class="row">
+                                          <p>للتواصل مع المعلن</p>
+                                          <div class="col-md-12"><button type="button" class="btn btn-danger btn-block">عرض رقم الموبايل</button></div>
+                                    </div>
+                              </div>
+                              <div class="col-md-12">
+                                    <dl class="dl-horizontal">
+
+                                          <dt>تفاصيل الإعلان : </dt>
+                                          <dd><?php echo $property_info->description; ?></dd>
+
+                                          <dt>الخدمات الملحقة : </dt>
+                                          <dd><?php echo $property_info->services; ?></dd>
+                                    </dl>
+                              </div>
+                        </div>
+                  </div>
+                  <div class="col-md-3">
+                        <div class="row">
+                              <div class="col-md-12 image-viewer">
+                                    <h4>صور العقار</h4>
+                                    <a href="#" class="thumbnail"><img src="<?php echo $property_info->image;?>" width="100%"></a>
+                                    <?php foreach($property_info->images as $image){ ?>
+                                    <div class="col-md-4 thumb"><a href="#"><img src="<?php echo $image->image_fullpath;?>" class="img-thumbnail"></a></div>
+                                    <?php } ?>
+                              </div>
+                              <div class="col-md-12">
+                                    <h4>الموقع على الخريطة</h4>
+                                    <img src="<?php echo $property_info->google_map_static_image; ?>" width="100%" class="img-thumbnail">
+                              </div>
+                        </div>
+                  </div>
+      	</div> <!-- /.property-view -->
+
+      </div> <!-- /.main -->
+
+    <?php echo $this->view("layouts/footer"); ?>
