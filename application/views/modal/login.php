f13e7cbff0b029ceb7af3a9f2d6b6769b75af1a6
saleh199 <saleh.saiid@gmail.com>
2014-08-01 22:40:11 +0300

    prevent add property if not logged in

---
+++ application/views/modal/login.php
@@ -0,0 +1,11 @@
+          <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.form.js');?>"></script>
+          <script type="text/javascript">
+            app.initializeAddProperty();
+          </script>
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4>تسجيل الدخول</h4>
+          </div>
+          <div class="modal-body">
+            يجب تسجيل الدخول قبل إضافة العقار <a href="<?php echo site_url('login');?>">تسجيل الدخول</a>
+          </div>
