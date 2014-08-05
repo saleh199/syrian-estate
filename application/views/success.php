bca2c3aaf292943e50df706501c2d99753c95189
saleh199 <saleh.saiid@gmail.com>
2014-08-06 00:44:30 +0300

    add success page

---
+++ application/views/success.php
@@ -0,0 +1,15 @@
+<?php echo $this->view("layouts/header", array("map" => false)); ?>
+
+      <div class="row main">
+            <div class="col-md-10 col-md-offset-2">
+                  <div class="page-header">
+                        <h3>تمت إرسال طلبك بنجاح</h3>
+                  </div>
+            </div>
+            <div class="col-md-8 col-md-offset-2">
+                  تم إرسال طلبك بنجاح سيقوم قسم التسويق بالتواصل معك لاستكمال البيانات الكاملة الخاصة بالمشروع
+            </div>
+
+      </div> <!-- /.main -->
+
+    <?php echo $this->view("layouts/footer"); ?>
